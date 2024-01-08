5116188a84704c1d6429775f52fd2f69d7b6a3ba
Valan012 <101065226+Valan012@users.noreply.github.com>
2024-01-08 17:59:41 +0000

    Add files via upload

---
+++ C111/copy _files.py
@@ -0,0 +1,5 @@
+import os 
+import shutil
+from_dir="C:/Users/navgh/Downloads/logo (1).PNG"
+to_dir="C:/Users/navgh/OneDrive/Desktop/Python Classes/C111"
+shutil.copy(from_dir,to_dir)
---
+++ C111/move_files.py
@@ -0,0 +1,5 @@
+import os 
+import shutil
+from_dir="C:/Users/navgh/Downloads/logo (1).PNG"
+to_dir="C:/Users/navgh/OneDrive/Desktop/Python Classes/C111"
+shutil.move(from_dir,to_dir)s
